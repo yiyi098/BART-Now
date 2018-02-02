407ec89796eeb694a5b6a7e95c1c2b97f606e44e
ehler orngard <ehlerorngard@calvisitor-10-105-128-130.calvisitor.1918.berkeley.edu>
2018-02-02 13:02:59 -0800

    added sidebar to HTML, CSS, and JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+// this toggles the settings sidebar ***
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    // when opening the sidebar
+    $('#sidebarCollapse').on('click', function () {
+        // open sidebar
+        $('#sidebar').addClass('active');
+        // fade in the overlay
+        $('.overlay').fadeIn();
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+   
+    // if dismiss or overlay was clicked
+    $('#dismiss, .overlay').on('click', function () {
+      // hide the sidebar
+      $('#sidebar').removeClass('active');
+      // fade out the overlay
+      $('.overlay').fadeOut();
+    });
+});
