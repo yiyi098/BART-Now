f2edf9832da24a02f2a229c3e9e8a4b4c385af55
yiyi098 <yiyi098@gmail.com>
2018-02-04 11:28:23 -0800

    added BART API calls for station list and info of trains of interest

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,67 @@ var config = {
 };
 
 firebase.initializeApp(config);
+
+var availableStations = [];
+
+var sampleStation = "San Bruno";
+
+var sampleDepartingTrains = [];
+
+var trainsOfInterest = [];
+
+function bartAvailableStationList() {
+
+  var bartApiKey = "ZJBQ-5E6T-9WWT-DWE9";
+  var jQueryURLAllStationInfo = "https://api.bart.gov/api/etd.aspx?cmd=etd&orig=all&json=y&key=" + bartApiKey;
+
+  $.ajax({
+  url: jQueryURLAllStationInfo,
+  method: "GET"
+  }).then(function(response) {
+    for (var i = 0; i < response.root.station.length; i++) {
+      availableStations.push(response.root.station[i].name);
+    }
+  });
+}
+
+bartAvailableStationList();
+
+console.log(availableStations);
+
+function checkStationOfInterest() {
+
+  var bartApiKey = "ZJBQ-5E6T-9WWT-DWE9";
+  var jQueryURLAllStationInfo = "https://api.bart.gov/api/etd.aspx?cmd=etd&orig=all&json=y&key=" + bartApiKey;
+
+  $.ajax({
+  url: jQueryURLAllStationInfo,
+  method: "GET"
+  }).then(function(response) {
+    for (var i = 0; i < response.root.station.length; i++) {
+      if (response.root.station[i].name === sampleStation) {
+        trainsOfInterest = response.root.station[i].etd;
+        console.log(trainsOfInterest);
+        // for (var i = 0; i < trainsOfInterest.length; i++) {
+          // sampleDepartingTrains.push(trainsOfInterest[i]);
+        // }
+      }
+    }
+  });
+}
+
+checkStationOfInterest();
+
+
+// console.log(sampleDepartingTrains);
+
+
+
+
+
+
+
+
+
+
+
