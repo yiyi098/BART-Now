721b2f80814ab26d1f53ca5fe979c0cac5d859c7
nwactor <nwactor@brandeis.edu>
2018-02-04 11:56:55 -0800

    start of putting user location into distance matrix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,17 +11,30 @@ firebase.initializeApp(config);
 var transportMode = 'walking';
 var defaultTimeLimit = 30;
 var clientLocation = '';
-var destination = ''; //will be provided from BART
+var destination;
+var bartStations = ['Rockridge+Bart', 'Downtown+Berkeley+Bart'];
 
-//geolocation API: get user location
-var locationApiKey = 'AIzaSyDz-OZA8ma5o7FDPJbEYDN9DgmU4OxBEOg';
-var locationRequest = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + locationApiKey;
-//https://code.tutsplus.com/tutorials/an-introduction-to-the-geolocation-api--cms-20071
-if (window.navigator && window.navigator.geolocation) {
-   console.log('something');
-} else {
-   // Not supported
+//get the location of all the bart locations
+var stationLocations = bartStations.join('|');
+
+//using built in HTML geolocation
+//https://dev.w3.org/geo/api/spec-source.html
+function locationError() {
+	console.log('Error: could not get location');
 }
+function geo_success(position) {
+  clientLocation = position.coords.latitude + ',' + position.coords.longitude;
+  console.log(clientLocation);
+}
+function geo_error() {
+  alert("Sorry, no position available.");
+}
+var geo_options = {
+  enableHighAccuracy: true, 
+  maximumAge        : 30000, 
+  timeout           : 27000
+};
+navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
 
 //distance API: get eta and distance
 var distanceMatrixApiKey = 'AIzaSyAmyDgw_JZ0uIEzvtYrt-550EhSy1ME5MU';
@@ -29,10 +42,18 @@ var distanceRequestUrlBase = 'https://maps.googleapis.com/maps/api/distancematri
 var distanceRequest;
 function updateDistanceRequest() {
 	distanceRequest = `${distanceRequestUrlBase}mode=${transportMode}&
-	origins=${clientLocation}&destination=${destination}&units=imperial&key=${distanceMatrixApiKey}`;
+	origins=${clientLocation}&destinations=${stationLocations}&units=imperial&key=${distanceMatrixApiKey}`;
 }
-distanceRequest = updateDistanceRequest();
+updateDistanceRequest();
+
+console.log(distanceRequest);
+
+// https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=enc:_kjwFjtsbMt%60EgnKcqLcaOzkGari%40naPxhVg%7CJjjb%40cqLcaOzkGari%40naPxhV:&key=YOUR_API_KEY
+// https://maps.googleapis.com/maps/api/distancematrix/json/?mode=walking&%20origins=&destination=Rockridge+Bart|Downtown+Berkeley+Bart&units=imperial&key=AIzaSyAmyDgw_JZ0uIEzvtYrt-550EhSy1ME5MU
+
+
+
 
 //map embed API: show map on page 2
-
 //directions API: show route on map
+
---
+++ assets/javascript/oldCode.js
@@ -0,0 +1,46 @@
+//geolocation API: get user location
+var locationApiKey = 'AIzaSyDz-OZA8ma5o7FDPJbEYDN9DgmU4OxBEOg';
+var locationRequest = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + locationApiKey;
+navigator.geolocation.getCurrentPosition(locationSuccess, locationError, {maximumAge:60000, enableHighAccuracy: true});
+function locationSuccess(position) {
+	clientLocation = position;
+	console.log(position);
+}
+function getLocation() {
+	gapi.client.init({
+		'apiKey': locationApiKey
+	}).then(function(){
+		return gapi.client.request({
+			'path' : locationRequest
+		})
+	}).then(function(response){
+		console.log(response.result);
+	}, function(reason) {
+		console.log('Error: ' + reason.result.error.message);
+	});
+}
+gapi.load('client', getLocation);
+//trying to use ajax
+$.ajax({
+	url: locationRequest,
+    method: "GET"
+}).done(function(response) {
+	console.log(locationRequest);
+	console.log(response);
+});
+//trying to use google
+//https://developers.google.com/api-client-library/javascript/start/start-js
+function getLocation() {
+	gapi.client.init({
+		'apiKey': locationApiKey
+	}).then(function(){
+		return gapi.client.request({
+			'path' : locationRequest
+		})
+	}).then(function(response){
+		console.log(response.result);
+	}, function(reason) {
+		console.log('Error: ' + reason.result.error.message);
+	});
+}
+gapi.load('client', getLocation);
