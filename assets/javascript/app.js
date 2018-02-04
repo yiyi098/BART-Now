318f1b0e1166a31cfde04eca3945f3eec0339522
nwactor <nwactor@brandeis.edu>
2018-02-03 21:51:42 -0800

    google api link and variables setting up some api queries

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,33 @@ var config = {
   storageBucket: "project-1-e130c.appspot.com",
   messagingSenderId: "1051617829929"
 };
-
 firebase.initializeApp(config);
 
-var googleMapsDistanceMatrixApiKey = 'AIzaSyAmyDgw_JZ0uIEzvtYrt-550EhSy1ME5MU';
+var transportMode = 'walking';
+var defaultTimeLimit = 30;
+var clientLocation = '';
+var destination = ''; //will be provided from BART
+
+//geolocation API: get user location
+var locationApiKey = 'AIzaSyDz-OZA8ma5o7FDPJbEYDN9DgmU4OxBEOg';
+var locationRequest = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + locationApiKey;
+//https://code.tutsplus.com/tutorials/an-introduction-to-the-geolocation-api--cms-20071
+if (window.navigator && window.navigator.geolocation) {
+   console.log('something');
+} else {
+   // Not supported
+}
+
+//distance API: get eta and distance
+var distanceMatrixApiKey = 'AIzaSyAmyDgw_JZ0uIEzvtYrt-550EhSy1ME5MU';
+var distanceRequestUrlBase = 'https://maps.googleapis.com/maps/api/distancematrix/json/?';
+var distanceRequest;
+function updateDistanceRequest() {
+	distanceRequest = `${distanceRequestUrlBase}mode=${transportMode}&
+	origins=${clientLocation}&destination=${destination}&units=imperial&key=${distanceMatrixApiKey}`;
+}
+distanceRequest = updateDistanceRequest();
+
+//map embed API: show map on page 2
+
+//directions API: show route on map
