39ec04c3556676be3c268ac4fc4f283145fa0239
nwactor <nwactor@brandeis.edu>
2018-02-07 15:07:45 -0800

    add dynamic countdown to navigation page

--- assets/javascript/navigationPage.js
+++ assets/javascript/navigationPage.js
@@ -3,10 +3,14 @@ currentTravelMode = window.sessionStorage.getItem('currentTravelMode');
 targetStation = JSON.parse(window.sessionStorage.getItem('targetStation'));
 selectedTrain = JSON.parse(window.sessionStorage.getItem('selectedTrain'));
 
+var trainCountdownIntervalID;
+
 $(document).ready(function() {
 	initMap();
     calculateAndDisplayRoute(directionsService, directionsDisplay);
 	$('#selectedTrain').append(createTraintainer(selectedTrain));
+
+	trainCountdownIntervalID = setInterval(countDown, 60000); 
 
 	$("#estimatedTravelTime").html(
 		'Estimated ' + currentTravelMode + ' time: <span id="minutesFromStation" class="minutesSpan"><span class="minsSpan">mins</span>');
@@ -14,3 +18,11 @@ $(document).ready(function() {
 });
 
 $("#navStationName").text(targetStation.name);
+
+function countDown() {
+	selectedTrain.eta--;
+	$('.traintainer').find('.minutesSpan').text(selectedTrain.eta);
+	if(selectedTrain.eta === 0) {
+		clearInterval(trainCountdownIntervalID);
+	}
+}
