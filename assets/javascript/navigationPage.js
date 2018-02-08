f03542dabaf85dcdab337e630dffb41ae2c8298f
ehler orngard <ehlerorngard@ehlers-MacBook.local>
2018-02-07 23:50:05 -0800

    fixed display issues, cleaned up code, added probability of catching the train

--- assets/javascript/navigationPage.js
+++ assets/javascript/navigationPage.js
@@ -1,5 +1,5 @@
 updatePreferenceUI();
-
+// draw data from session storage from the inital page
 clientLocation = window.sessionStorage.getItem('clientLocation');
 currentTravelMode = window.sessionStorage.getItem('currentTravelMode');
 targetStation = JSON.parse(window.sessionStorage.getItem('targetStation'));
@@ -8,12 +8,13 @@ var distance = targetStation.distance.text;
 var travelTime = targetStation.travelTime.text;
 var backButton = $("<button class='backButton'>");
 backButton.text("back to trains");
-
 backButton.on("click", function(){
 	window.location.replace("https://yiyi098.github.io/BART-Now/");
 });
 
 var trainCountdownIntervalID;
+var chance;
+var travelTimeInt = parseInt(travelTime.slice(0, travelTime.indexOf("m")-1));
 
 $(document).ready(function() {
 	initMap();
@@ -22,13 +23,28 @@ $(document).ready(function() {
 
 	trainCountdownIntervalID = setInterval(countDown, 60000); 
 	
-  $("#distanceToBart").html(
+  	$("#distanceToBart").html(
 		'Distance to BART station: <span id="distance">' + distance + '</span>');
 
 	$("#estimatedTravelTime").html(
 		'Estimated ' + currentTravelMode + ' time: <span id="minutesFromStation" class="minutesSpan">' + travelTime + '</span>');
+// to determine a rough chance of being able to catch the selected train
+	if (travelTimeInt > selectedTrain.eta+1) {
+		chance = "low";
+		$("#estimatedChance").html(
+			'Chance of catching this train: <span class="chance" id=' + chance + '>LOW</span>');
+	}
+	else if (travelTimeInt >= selectedTrain.eta-1) {
+		chance = "medium";
+		$("#estimatedChance").html(
+			'Catch it? <span class="chance" id=' + chance + '>cutting it close...</span>');
+	}
+	else {
+		chance = "high";
+		$("#estimatedChance").html(
+			'Chance of catching this train: <span class="chance" id=' + chance + '>high</span>');
+	}
 	$("#backButtonDiv").append(backButton);
-
 });
 
 $("#navStationName").text(targetStation.name);
